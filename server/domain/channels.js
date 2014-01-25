7c2db7ded968d8cb9aa49b7fe2ecf8f6fdcc0cf8
Aditya Sharat <adityasharat@gmail.com>
2014-01-25 17:47:37 +0530

    update to the channel model

--- server/domain/channels.js
+++ server/domain/channels.js
@@ -10,17 +10,36 @@ module.exports = function (db, BSON) {
     var dataStore = require('../framework/dataStore')(db, BSON, 'channels', 'channel');
 
     return {
-        create : function (channel, callback) {
-
+        create : function (feed, callback) {
+            try {
+                dataStore.add(feed, callback);
+            } catch (error) {
+                console.error(error.message);
+            }
         },
+
         read : function (condition, callback) {
-
+            try {
+                dataStore.find(condition, callback);
+            } catch (error) {
+                console.error(error.message);
+            }
         },
-        update : function (condition, callback) {
 
+        update : function (condition, item, callback) {
+            try {
+                dataStore.update(condition, item, callback);
+            } catch (error) {
+                console.error(error.message);
+            }
         },
+
         delete : function (condition, callback) {
-
-        },
+            try {
+                dataStore.delete(condition, callback);
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
     };
 };
